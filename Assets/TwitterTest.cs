359806c5a39aebdc011390899ed4c5341b198944
NickWeintraut <weintraun8@students.rowan.edu>
2017-02-04 15:53:05 -0500

    Initial commit with full VR settings for oculus and leap motion support.

---
+++ Assets/TwitterTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterTest : MonoBehaviour {
+
+    public string url = "http://www.twitter.com";
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        TextMesh text = GetComponent<TextMesh>();
+        text.text = www.text;
+        print("testt");
+    }
+}
